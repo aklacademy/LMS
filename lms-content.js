67bd6605f4ba9df31999d9bfdfcc3af31f8948d4
aklacademy <kuttya1611@gmail.com>
2026-05-25 17:38:19 +0530

    Add files via upload

---
+++ lms-content.js
@@ -0,0 +1,53 @@
+const vocabularyThemes = {
+
+    "Office Communication": [
+
+        "General Workplace Communication",
+        "Roles & Tasks",
+        "Performance & Growth"
+    ],
+    
+    "Food & Eating Out": [   
+
+        "Restaurant Vocabulary",
+        "Cooking Vocabulary",
+        "Ordering Food"
+    ],
+
+    "Travel & Directions": [
+
+        "Airport Vocabulary",
+        "Hotel Vocabulary",
+        "Asking Directions"
+
+    ]
+
+
+    
+};
+
+const englishThemes = {
+
+    "Grammar": [
+
+        "Present Simple",
+        "Past Simple",
+        "Articles"
+    ],
+
+    "Idioms": [
+
+        "Success & Failure",
+        "Communication",
+        "Money & Business"
+    ],
+
+   "Phrases": [  
+    
+        "Greetings",
+        "Requests",
+        "Opinions"
+
+    ]
+};
+
