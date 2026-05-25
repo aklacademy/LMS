9ee2d963096737102a74bd02e9161b8869aacdf4
aklacademy <kuttya1611@gmail.com>
2026-05-25 17:39:10 +0530

    Add files via upload

---
+++ lms-questions.js
@@ -0,0 +1,39 @@
+const assessmentQuestions = {
+
+    "General Workplace Communication": [
+
+        {
+            questionId: "WJV1Q01",
+
+            question:
+            "The company plans to ______ new employees next month.",
+
+            options: [
+                "hire",
+                "document",
+                "negotiate",
+                "orient"
+            ],
+
+            correctAnswer: "hire"
+        },
+
+        {
+            questionId: "WJV1Q02",
+
+            question:
+            "The technology firm wants to ______ engineering graduates.",
+
+            options: [
+                "recruit",
+                "authorize",
+                "delegate",
+                "confirm"
+            ],
+
+            correctAnswer: "recruit"
+        }
+
+    ]
+
+};
