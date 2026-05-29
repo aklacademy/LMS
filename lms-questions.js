b4aecd9bfe7851416277dced915376b89f023097
aklacademy <kuttya1611@gmail.com>
2026-05-29 16:59:42 +0530

    Add files via upload

--- lms-questions.js
+++ lms-questions.js
@@ -1,39 +1,137 @@
 const assessmentQuestions = {
 
-    "General Workplace Communication": [
+    "Forest Words": {
 
-        {
-            questionId: "WJV1Q01",
+        "Set 1": [
 
-            question:
-            "The company plans to ______ new employees next month.",
+            {
+                questionId: "0001",
+                category: "main",
+                contentId: "WJV1",
 
-            options: [
-                "hire",
-                "document",
-                "negotiate",
-                "orient"
-            ],
+                question:
+                "The company plans to ______ new employees next month.",
 
-            correctAnswer: "hire"
-        },
+                options: [
+                    "hire",
+                    "document",
+                    "negotiate",
+                    "orient"
+                ],
 
-        {
-            questionId: "WJV1Q02",
+                correctAnswer: "hire"
+            },  
+            
+            {
+                questionId: "0002",
+                category: "main",
+                contentId: "WJV2",
 
-            question:
-            "The technology firm wants to ______ engineering graduates.",
+                question:
+                "The technology firm wants to ______ engineering graduates.",
 
-            options: [
-                "recruit",
-                "authorize",
-                "delegate",
-                "confirm"
-            ],
+                options: [
+                    "recruit",
+                    "authorize",
+                    "delegate",
+                    "confirm"
+                ],
 
-            correctAnswer: "recruit"
-        }
+                correctAnswer: "recruit"
+            },
 
-    ]
+            {
+                questionId: "0003",
+                category: "main",
+                contentId: "WJV3",
+
+                question:
+                "She ______ with pressure w",
+
+                options: [
+                    "coped",
+                    "authorize",
+                    "delegate",
+                    "confirm"
+                ],
+
+                correctAnswer: "coped"
+
+            }
+
+        ],
+
+        "Set 2": [
+
+            {
+                questionId: "0004",
+                category: "main",
+                contentId: "WJV1",
+
+                question:
+                "The manager decided to ______ three new interns.",
+
+                options: [
+                    "hire",
+                    "schedule",
+                    "organize",
+                    "review"
+                ],
+
+                correctAnswer: "hire"
+            }
+
+        ],
+
+        "Day 3": [
+
+            {
+                questionId: "M005",
+
+                category: "mastered",
+
+                contentId: "WJV1",
+
+                question:
+                "What does 'hire' mean?",
+
+                options: [
+                    "Employ",
+                    "Dismiss",
+                    "Ignore",
+                    "Escape"
+                ],
+
+                correctAnswer: "Employ"
+            },
+
+            {
+
+                questionId: "M006",
+                category: "mastered",
+                contentId: "WJV1",
+
+                question: 
+
+                "My friend decided to ______________ me",
+
+                options: [
+
+                    "hire",
+                    "fire",
+                    "select",
+                    "reject"
+                ],
+
+                correctAnswer: "hire"
+
+            
+            }
+
+        ]
+
+    }
 
 };
+
+
