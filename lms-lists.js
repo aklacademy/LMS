0dc0a26f5f0cce428e676eabc2d098eec6c77dab
aklacademy <kuttya1611@gmail.com>
2026-05-29 16:59:23 +0530

    Add files via upload

---
+++ lms-lists.js
@@ -0,0 +1,33 @@
+window.lists = [
+
+    {
+
+        listId: "LIST001",
+
+        themeTitle: "Nature Vocabulary",
+
+        title: "Forest Words"
+
+    },
+
+    {
+
+        listId: "LIST002",
+
+        themeTitle: "Nature Vocabulary",
+
+        title: "Water Words"
+
+    },
+
+    {
+
+        listId: "LIST003",
+
+        themeTitle: "Conditional Statements",
+
+        title: "Conditional Statements"
+
+    }
+
+];
