545cee499382dfab5a3033a2311dc2ae7203098b
aklacademy <kuttya1611@gmail.com>
2026-05-29 16:59:04 +0530

    Add files via upload

---
+++ lms-courses.js
@@ -0,0 +1,19 @@
+window.courses = [
+
+    {
+
+        courseId: "COURSE001",
+
+        title: "Spoken English",
+
+        learningAreas: [
+
+            "Vocabulary",
+            "Grammar",
+            "Communication Skills"
+
+        ]
+
+    }
+
+];
