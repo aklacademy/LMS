90678e0dbe8b7fcab4823e7af97cc29246fff9bf
aklacademy <kuttya1611@gmail.com>
2026-05-29 17:00:33 +0530

    Add files via upload

---
+++ lms-themes.js
@@ -0,0 +1,33 @@
+window.themes = [
+
+    {
+
+        themeId: "TH001",
+
+        learningArea: "Vocabulary",
+
+        title: "Nature Vocabulary"
+
+    },
+
+    {
+
+        themeId: "TH002",
+
+        learningArea: "Vocabulary",
+
+        title: "Workplace Vocabulary"
+
+    },
+
+    {
+
+        themeId: "TH003",
+
+        learningArea: "Grammar",
+
+        title: "ConditionalConditional Statements"
+
+    }
+
+];
