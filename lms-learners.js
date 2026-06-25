fab6bab0c75f99f26405359647abf011cda40561
aklacademy <kuttya1611@gmail.com>
2026-06-25 17:35:18 +0530

    Add files via upload

--- lms-learners.js
+++ lms-learners.js
@@ -1,33 +1,5 @@
 let learners = [];
 
-function loadLearners() {
-
-    const savedLearners =
-        localStorage.getItem(
-            "learners"
-        );
-
-    if (savedLearners) {
-
-        learners =
-            JSON.parse(
-                savedLearners
-            );
-
-    }
-
-}
-
-function saveLearners() {
-
-    localStorage.setItem(
-        "learners",
-        JSON.stringify(
-            learners
-        )
-    );
-
-}
 
 
 function getNextLearnerId() {
